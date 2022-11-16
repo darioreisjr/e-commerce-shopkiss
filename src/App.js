0342c3c3dc7823e5cea583c852b6be2360223c30
Dario Reis <guttyerresjunior1995@gmail.com>
2022-11-15 21:09:22 -0300

    adiciona configuração de totas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
 import './App.css'
 
+import { createBrowserRouter } from "react-router-dom";
+
 import Login from './pages/Login';
 
-function App() {
-  return (
-    <div className="App">
-     
-      <Login/>
-      
-    </div>
-  );
-}
+const router = createBrowserRouter([
+ 
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  
+ 
+ 
+]);
 
-export default App;
+export { router };
